b24fec9abe5b0573bba7261bd62e02bfd66abd87
appHelge <appHelge@users.noreply.github.com>
2015-07-25 10:43:36 +0200

    Add .gitignore and project files

---
+++ WeathermanTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeathermanTest
+//
+//  Created by Helge Larsen on 25.07.15.
+//  Copyright © 2015 Helge Larsen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ WeathermanTestTests/WeathermanTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeathermanTestTests.swift
+//  WeathermanTestTests
+//
+//  Created by Helge Larsen on 25.07.15.
+//  Copyright © 2015 Helge Larsen. All rights reserved.
+//
+
+import XCTest
+@testable import WeathermanTest
+
+class WeathermanTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
